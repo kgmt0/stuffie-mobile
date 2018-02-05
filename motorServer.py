f8ab9889fd61646666e33426442534f49d77b461
kgmt0 <kritphong@teknik.io>
2018-02-05 07:34:57 +0000

    Fix errors in motorServer

--- motorServer.py
+++ motorServer.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 import RPi.GPIO as GPIO
 from time import sleep
+import atexit
 
 GPIO.setmode(GPIO.BOARD)
  
@@ -16,51 +17,51 @@ GPIO.setup(Motor1E,GPIO.OUT)
 app = Flask(__name__)
 
 def forward():
-	print "forward"
+	print("forward")
 	GPIO.output(Motor1A,GPIO.HIGH)
 	GPIO.output(Motor1B,GPIO.LOW)
 	GPIO.output(Motor1E,GPIO.HIGH)
 	sleep(2)
-	print "Stopping motor"
+	print("Stopping motor")
 	GPIO.output(Motor1E,GPIO.LOW)
 
 def backwards():
-	print "backwards"
+	print("backwards")
 	GPIO.output(Motor1A,GPIO.LOW)
 	GPIO.output(Motor1B,GPIO.HIGH)
 	GPIO.output(Motor1E,GPIO.HIGH)
 	sleep(2)
-	print "Stopping motor"
+	print("Stopping motor")
 	GPIO.output(Motor1E,GPIO.LOW)
 
 def runMotors(commandsVec):
-	for command in commandsVec:
-		if command == 'up':
-			forward()
-		if command == 'down':
-			backwards()
+    for command in commandsVec:
+            if command == 'up':
+                    forward()
+            if command == 'down':
+                    backwards()
 
 @app.route('/start/')
 def start():
-	print'start'
+    print('start')
     return "Hello World!"
 
 @app.route('/stop/')
 def stop():
-	print 'stop'
+    print('stop')
     return "goodbye World!"
 
 @app.route('/load-command/<commands>')
 def load(commands):
     commandsVec = commands.split('-')
     runMotors(commandsVec)
-    return
+    return "hello"
 
 @app.route('/running/')
 def running():
-	print 'running'
+    print('running')
     return "goodbye World!"
 
+atexit.register(GPIO.cleanup)
 
-app.run()
-GPIO.cleanup()
+# app.run()
